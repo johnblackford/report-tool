5a891aa0612a07f2b564952893dd748eb5b6b3d8
John Blackford <john.blackford@gmail.com>
2015-06-11 16:21:39 -0500

    Fixes for pylint against debugValidators.py

--- debugValidators.py
+++ debugValidators.py
@@ -19,21 +19,21 @@ from abstract_classes import AbstractValidator
 
 
 class DocDescValidator(AbstractValidator):
-	"""Debugging Validator - validates the Document Description is not empty"""
+    """Debugging Validator - validates the Document Description is not empty"""
 
-	def validate(self, document):
-		output_buffer = cStringIO.StringIO()
-		logger = logging.getLogger(self.__class__.__name__)
+    def validate(self, document):
+        output_buffer = cStringIO.StringIO()
+        logger = logging.getLogger(self.__class__.__name__)
 
-		logger.info("Starting Validator...")
+        logger.info("Starting Validator...")
 
-		if len(document.get_description()) == 0:
-			out_buffer.write("Validation Failure: WARN: Document Description is empty")
-			print out_buffer.getvalue()
-			logger.warn(out_buffer.getvalue())
-		else:
-			logger.debug("All Validation Passed")
+        if len(document.get_description()) == 0:
+            output_buffer.write("Validation Failure: WARN: Document Description is empty")
+            print output_buffer.getvalue()
+            logger.warn(output_buffer.getvalue())
+        else:
+            logger.debug("All Validation Passed")
 
-		logger.info("Validator Complete")
-			
-		output_buffer.close()
+        logger.info("Validator Complete")
+
+        output_buffer.close()
